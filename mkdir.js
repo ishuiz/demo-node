6e4776d261d5e1403e4ba239c9440eaf8c1c1287
Huiz <1057354161@qq.com>
2019-09-17 21:50:04 +0800

    feat: rename & rmdir

--- mkdir.js
+++ mkdir.js
@@ -3,5 +3,24 @@ const fs = require('fs')
 fs.mkdir('./test', (err) => {
   if (err) {
     console.log(err)
+    return
   }
+  console.log('创建成功')
+})
+
+// fs.rename('./test', './test01', (err) => {
+//   if (err) {
+//     console.log(err)
+//     return
+//   }
+//   console.log('修改成功')
+// })
+
+// 只能删除空的
+fs.rmdir('./test01', (err) => {
+  if (err) {
+    console.log(err)
+    return
+  }
+  console.log('删除成功')
 })
