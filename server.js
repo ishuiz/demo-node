edb651069af8b9d593b334b8dd0fd509149d6750
Huiz <1057354161@qq.com>
2019-10-09 21:26:03 +0800

    feat: server

--- server.js
+++ server.js
@@ -1,10 +1,36 @@
 const express = require('express')
 
+const bodyParser = require('body-parser')
+
 const app = express()
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 // http://localhost:3000/user/login
 app.get('/user/login', (req, res) => {
-  res.send({err: '000000', msg: 'ok'})
+  console.log(req.query)
+  const { us, ps } = req.query
+  if (us === 'lee' && ps === '123') {
+    res.send({err: '000000', msg: 'ok'})
+  } else {
+    res.send({err: '000001', msg: 'oh no!'})
+  }
+})
+
+app.get('/test', (req, res) => {
+  res.send('test ok')
+})
+
+app.post('/user/reg', (req, res) => {
+  // express 不能直接解析消息体, 需要借助 body-parser
+  console.log(req.body)
+  const { us, ps } = req.body
+  if (us === '123' && ps === '123') {
+    res.send({err: '000000', msg: 'ok'})
+  } else {
+    res.send({err: '000001', msg: 'oh no!'})
+  }
 })
 
 app.listen(3000, () => {
